60247ee43e59a2712590b1b3f570649a22ce485e
Florian Heinemann <florian.heinemann@gmail.com>
2014-03-04 21:39:29 +0100

    Fixed issue with auto-save

--- public/javascripts/controllers/WolkentaskController.js
+++ public/javascripts/controllers/WolkentaskController.js
@@ -1,275 +1,280 @@
 "use strict";
 function WolkentaskController($scope, $http, $q, $window, $timeout, favoritesService, exceptionService, dropboxClientService) {
 
-    var saveQueue = false;
-    var saveDelayTimer = null;
-    $scope.showNavMenu = true;
-    $scope.filesAndFolders = [];
-    $scope.parentFolder = '';
-    $scope.currentFolder = '';
-    $scope.currentFilePath = '';
-    $scope.currentFileName = '';
-    $scope.currentFileData = [];
-    $scope.currentFileVersion = '';
-    $scope.favorites = [];
-    $scope.backString = "..";
-    $scope.newTodo = {
-        rawData: ""
-    };
-    $scope.saveStatus = "saved";
+	var saveQueue = false;
+	var saveDelayTimer = null;
+	$scope.showNavMenu = true;
+	$scope.filesAndFolders = [];
+	$scope.parentFolder = '';
+	$scope.currentFolder = '';
+	$scope.currentFilePath = '';
+	$scope.currentFileName = '';
+	$scope.currentFileData = [];
+	$scope.currentFileVersion = '';
+	$scope.favorites = [];
+	$scope.backString = "..";
+	$scope.newTodo = {
+		rawData: ""
+	};
+	$scope.saveStatus = "saved";
 
-    $scope.$on('error', function(name, errorType, errorAction, message) {
-        alert(message);
+	$scope.$on('error', function(name, errorType, errorAction, message) {
+		alert(message);
 
-        if(errorAction === 2) {
-            $window.location.href = '/logout';
-        }
-    });
+		if(errorAction === 2) {
+			$window.location.href = '/logout';
+		}
+	});
 
-    $scope.logout = function() {
-        $window.location.href = '/logout';
-    }
+	$scope.logout = function() {
+		$window.location.href = '/logout';
+	}
 
-    $scope.initialize = function(providerId, providerToken, dropboxAppKey) {
+	$scope.initialize = function(providerId, providerToken, dropboxAppKey) {
 
-        dropboxClientService.initialize(providerId, providerToken, dropboxAppKey);
+		dropboxClientService.initialize(providerId, providerToken, dropboxAppKey);
 
-        dropboxClientService.trackChanges().then(function() {}, 
-            function(error) {
-                exceptionService.raiseError(error);
-            }, 
-            function(changedFiles) {
+		dropboxClientService.trackChanges().then(function() {}, 
+			function(error) {
+				exceptionService.raiseError(error);
+			}, 
+			function(changedFiles) {
 
-                // Avoid refresh of file list if the only change is an update of the currently opened file
-                if(changedFiles.changes.length !== 1 ||
-                    changedFiles.changes[0].path.toLowerCase() !== $scope.currentFilePath.toLowerCase() || 
-                    changedFiles.changes[0].wasRemoved) {
-                        $scope.requestFolderContent($scope.currentFolder);
-                }
+				// Avoid refresh of file list if the only change is an update of the currently opened file
+				if(changedFiles.changes.length !== 1 ||
+					changedFiles.changes[0].path.toLowerCase() !== $scope.currentFilePath.toLowerCase() || 
+					changedFiles.changes[0].wasRemoved) {
+						$scope.requestFolderContent($scope.currentFolder);
+				}
 
-                // Refresh currently shown file to latest version if not edited
-                if($scope.currentFileName) {
-                    dropboxClientService.fileMetaData($scope.currentFilePath).then(function(fileStat) {
-                        if($scope.currentFileVersion !== fileStat.versionTag) {
-                            if($scope.saveStatus === "saved") {
-                                $scope.getFile($scope.currentFilePath);
-                            }
-                        }
-                    });
-                }
-            });
+				// Refresh currently shown file to latest version if not edited
+				if($scope.currentFileName) {
+					dropboxClientService.fileMetaData($scope.currentFilePath).then(function(fileStat) {
+						if($scope.currentFileVersion !== fileStat.versionTag) {
+							if($scope.saveStatus === "saved") {
+								$scope.getFile($scope.currentFilePath);
+							}
+						}
+					});
+				}
+			});
 
-        $scope.requestFolderContent('');
+		$scope.requestFolderContent('');
 
-        favoritesService.receiveFavorites(true).then(function(data) {
-            $scope.favorites = data;
-        });
-    };
+		favoritesService.receiveFavorites(true).then(function(data) {
+			$scope.favorites = data;
+		});
+	};
 
-    $scope.isInRootFolder = function() {
-        return (!$scope.currentFolder || $scope.currentFolder == "" || $scope.currentFolder == "." || $scope.currentFolder == "/");
-    }
+	$scope.isInRootFolder = function() {
+		return (!$scope.currentFolder || $scope.currentFolder == "" || $scope.currentFolder == "." || $scope.currentFolder == "/");
+	}
 
-    $scope.requestFolderContent = function(path) {
-        var deferred = $q.defer();
+	$scope.requestFolderContent = function(path) {
+		var deferred = $q.defer();
 
-        dropboxClientService.listDirectoryContent(path).then(
-            function(data) {
-                $scope.filesAndFolders = data;
-                $scope.parentFolder = new Path(path).dirname();
-                $scope.currentFolder = path;
-                deferred.resolve();
-            }, function(error) {
-                exceptionService.raiseError(error);
-                deferred.reject();
-            });
+		dropboxClientService.listDirectoryContent(path).then(
+			function(data) {
+				$scope.filesAndFolders = data;
+				$scope.parentFolder = new Path(path).dirname();
+				$scope.currentFolder = path;
+				deferred.resolve();
+			}, function(error) {
+				exceptionService.raiseError(error);
+				deferred.reject();
+			});
 
-        return deferred.promise;
-    };
+		return deferred.promise;
+	};
 
-    $scope.createNewFile = function(path) {
-        var deferred = $q.defer();
-        dropboxClientService.writeFile(path, "- [ ] Your first todo", 
-                { noOverwrite: true }).then(
-                function(success) {
-                    $scope.requestFolderContent($scope.currentFolder).then(function(fileStat) {
-                        deferred.resolve();
-                    });
-                }, function(error) {
-                    exceptionService.raiseError(error);
-                    deferred.reject();
-                });
-        return deferred.promise;
-    };
+	$scope.createNewFile = function(path) {
+		var deferred = $q.defer();
+		dropboxClientService.writeFile(path, "- [ ] Your first todo", 
+				{ noOverwrite: true }).then(
+				function(success) {
+					$scope.requestFolderContent($scope.currentFolder).then(function(fileStat) {
+						deferred.resolve();
+					});
+				}, function(error) {
+					exceptionService.raiseError(error);
+					deferred.reject();
+				});
+		return deferred.promise;
+	};
 
-    $scope.getFile = function(path) {        
-        var deferred = $q.defer();
+	$scope.getFile = function(path) {        
+		var deferred = $q.defer();
 
-        dropboxClientService.readFile(path).then(function(success) {
-            $scope.currentFilePath = path;
-            $scope.currentFileName = new Path(path).basename();
-            $scope.currentFileData = parseFile(success.data);
-            $scope.currentFileVersion = success.fileStat.versionTag;
-            $scope.showNavMenu = false;
-            $scope.saveStatus = "saved";  
-            if(saveDelayTimer)
-                $timeout.cancel(saveDelayTimer);
-            saveQueue = false;
-            deferred.resolve();         
-        }, function(error) {
-            exceptionService.raiseError(error);
-            deferred.reject();
-        });
+		dropboxClientService.readFile(path).then(function(success) {
+			$scope.currentFilePath = path;
+			$scope.currentFileName = new Path(path).basename();
+			$scope.currentFileData = parseFile(success.data);
+			$scope.currentFileVersion = success.fileStat.versionTag;
+			$scope.showNavMenu = false;
+			$scope.saveStatus = "saved";  
+			if(saveDelayTimer)
+				$timeout.cancel(saveDelayTimer);
+			saveQueue = false;
+			deferred.resolve();         
+		}, function(error) {
+			exceptionService.raiseError(error);
+			deferred.reject();
+		});
 
-        return deferred.promise;
-    };
+		return deferred.promise;
+	};
 
-    function parseFile(dataAsString) {
+	function parseFile(dataAsString) {
 
-        var parsedData = [];
-        var splittedString = dataAsString.split("\n");
-        splittedString.forEach(function(line) {
-            var regexToFindTodo = /^\s*-*\s*\[\s*_*(x|X)?\s*\]/g;
-            var regexToFindLine = /^\s*-*\s*/g;
+		var parsedData = [];
+		var splittedString = dataAsString.split("\n");
+		splittedString.forEach(function(line) {
+			var regexToFindTodo = /^\s*-*\s*\[\s*_*(x|X)?\s*\]/g;
+			var regexToFindLine = /^\s*-*\s*/g;
 
-            var parsedDataLine = {  rawData: line,
-                                displayData: line.trim(),
-                                display: true,
-                                inTodoFormat : false,
-                                done : false,
-                                changed : false };
+			var parsedDataLine = {  rawData: line,
+								displayData: line.trim(),
+								display: true,
+								inTodoFormat : false,
+								done : false,
+								changed : false };
 
-            if(parsedDataLine.displayData == "") {
-               parsedDataLine.display = false;
-            } else {
-                var regexResults = regexToFindTodo.exec(parsedDataLine.displayData);
-                parsedDataLine.inTodoFormat = regexResults != null;
-                if(parsedDataLine.inTodoFormat) {
-                    parsedDataLine.displayData = parsedDataLine.displayData.substr(regexToFindTodo.lastIndex).trim();
-                    parsedDataLine.done = !(typeof regexResults[1] === "undefined");
-                } else {
-                    var match = regexToFindLine.exec(parsedDataLine.displayData);
-                    if(match) 
-                        parsedDataLine.displayData = parsedDataLine.displayData.substr(regexToFindLine.lastIndex).trim();
-                }
-            }
+			if(parsedDataLine.displayData == "") {
+			   parsedDataLine.display = false;
+			} else {
+				var regexResults = regexToFindTodo.exec(parsedDataLine.displayData);
+				parsedDataLine.inTodoFormat = regexResults != null;
+				if(parsedDataLine.inTodoFormat) {
+					parsedDataLine.displayData = parsedDataLine.displayData.substr(regexToFindTodo.lastIndex).trim();
+					parsedDataLine.done = !(typeof regexResults[1] === "undefined");
+				} else {
+					var match = regexToFindLine.exec(parsedDataLine.displayData);
+					if(match) 
+						parsedDataLine.displayData = parsedDataLine.displayData.substr(regexToFindLine.lastIndex).trim();
+				}
+			}
 
-            parsedData.push(parsedDataLine);
-        });
+			parsedData.push(parsedDataLine);
+		});
 
-        return parsedData;
-    };
+		return parsedData;
+	};
 
-    $scope.addNewTodo = function() {
-        if($scope.newTodo.rawData && $scope.currentFileData) {
-            var newTodoData = {  rawData: $scope.newTodo.rawData,
-                                displayData: $scope.newTodo.rawData.trim(),
-                                display: true,
-                                inTodoFormat : true,
-                                done : false,
-                                changed : true };
+	$scope.addNewTodo = function() {
+		if($scope.newTodo.rawData && $scope.currentFileData) {
+			var newTodoData = {  rawData: $scope.newTodo.rawData,
+								displayData: $scope.newTodo.rawData.trim(),
+								display: true,
+								inTodoFormat : true,
+								done : false,
+								changed : true };
 
-            $scope.currentFileData.unshift(newTodoData);
-            $scope.newTodo.rawData = "";
-            fileChanged();
-        }
-    };
+			$scope.currentFileData.unshift(newTodoData);
+			$scope.newTodo.rawData = "";
+			fileChanged();
+		}
+	};
 
-    $scope.markAllTodosAs = function(ticked) {
-        if(!$scope.currentFileData)
-            return;
+	$scope.markAllTodosAs = function(ticked) {
+		if(!$scope.currentFileData)
+			return;
 
-        var anyChange = false;
-        $scope.currentFileData.forEach(function(todo) {
-            if(todo.done !== ticked) {
-                todo.changed = true;
-                todo.done = ticked;
-                anyChange = true;
-            }
-        });
+		var anyChange = false;
+		$scope.currentFileData.forEach(function(todo) {
+			if(todo.done !== ticked) {
+				todo.changed = true;
+				todo.done = ticked;
+				anyChange = true;
+			}
+		});
 
-        if(anyChange)
-            fileChanged();
-    };
+		if(anyChange)
+			fileChanged();
+	};
 
-    $scope.updateTodo = function(todo) {
-        if($scope.currentFileData) {
-            todo.changed = true;
-            todo.rawData = todo.displayData;
-            fileChanged();
-        }
-    };
+	$scope.updateTodo = function(todo) {
+		if($scope.currentFileData) {
+			todo.changed = true;
+			todo.rawData = todo.displayData;
+			fileChanged();
+		}
+	};
 
-    $scope.deleteTodo = function(todo) {
-        if($scope.currentFileData) {
-            var indexOfTodo = $scope.currentFileData.indexOf(todo);
-            if(indexOfTodo >= 0) {
-                $scope.currentFileData.splice(indexOfTodo, 1);
-                fileChanged();
-            }
-        }
-    };
+	$scope.deleteTodo = function(todo) {
+		if($scope.currentFileData) {
+			var indexOfTodo = $scope.currentFileData.indexOf(todo);
+			if(indexOfTodo >= 0) {
+				$scope.currentFileData.splice(indexOfTodo, 1);
+				fileChanged();
+			}
+		}
+	};
 
-    function fileChanged() {
-        $scope.saveStatus = "unsaved";
+	function fileChanged() {
+		$scope.saveStatus = "unsaved";
+		queueSave();
+	};
 
-        if(!saveQueue) {
-            saveQueue = true;
-            
-            $timeout(function() {}, 5000).then(function() {
-                return $scope.saveFile();
-            }).finally(function() {
-                saveQueue = false;
-            });
-        } else {
-            // No point in backlogging several timers
-            if(saveDelayTimer)
-                $timeout.cancel(saveDelayTimer);
-            saveDelayTimer = $timeout(function() { fileChanged(); }, 1000);
-        }
-    };
+	function queueSave() {
+		if(!saveQueue) {
+			saveQueue = true;
+			$timeout(function() {}, 5000).then(function() {
+				$scope.saveFile().finally(function() {
+					saveQueue = false;
+				});
+			});
+		} else {
+			// No point in backlogging several timers
+			if(saveDelayTimer) {
+				$timeout.cancel(saveDelayTimer);
+			}
 
-    $scope.saveFile = function() {
-        var deferred = $q.defer();
+			saveDelayTimer = $timeout(function() { queueSave(); }, 1000);
+		}		
+	}
 
-        if($scope.currentFileData && $scope.saveStatus === "unsaved") {
-            $scope.saveStatus = "saving";
+	$scope.saveFile = function() {
+		var deferred = $q.defer();
 
-            var dataToWrite = "";
-            $scope.currentFileData.forEach(function(line) {
-                if(line.changed) {
-                    if(line.done) {
-                        dataToWrite += "- [x] " + line.displayData + "\n";
-                    } else {
-                        dataToWrite += "- [ ] " + line.displayData + "\n";
-                    }
-                } else {
-                    dataToWrite += line.rawData + "\n";
-                }
-            });
+		if($scope.currentFileData && $scope.saveStatus === "unsaved") {
+			$scope.saveStatus = "saving";
 
-            if(dataToWrite.length > 0)
-                dataToWrite = dataToWrite.substr(0, dataToWrite.length - 1);
+			var dataToWrite = "";
+			$scope.currentFileData.forEach(function(line) {
+				if(line.changed) {
+					if(line.done) {
+						dataToWrite += "- [x] " + line.displayData + "\n";
+					} else {
+						dataToWrite += "- [ ] " + line.displayData + "\n";
+					}
+				} else {
+					dataToWrite += line.rawData + "\n";
+				}
+			});
 
-            dropboxClientService.writeFile($scope.currentFilePath, dataToWrite, 
-                        { lastVersionTag: $scope.currentFileVersion }).then(
-                            function(fileStat) {
-                                $scope.currentFileVersion = fileStat.versionTag;
-                                $scope.saveStatus = "saved";
-                            }, function(error) {
-                                exceptionService.raiseError(error);
-                                $scope.saveStatus = "unsaved";
-                            }).finally(function() {
-                                deferred.resolve();
-                            });
+			if(dataToWrite.length > 0)
+				dataToWrite = dataToWrite.substr(0, dataToWrite.length - 1);
 
-            return deferred.promise;
-        } else {
-            return $q.reject("No data to be saved.");
-        }
-    };
+			dropboxClientService.writeFile($scope.currentFilePath, dataToWrite, 
+						{ lastVersionTag: $scope.currentFileVersion }).then(
+							function(fileStat) {
+								$scope.currentFileVersion = fileStat.versionTag;
+								$scope.saveStatus = "saved";
+							}, function(error) {
+								exceptionService.raiseError(error);
+								$scope.saveStatus = "unsaved";
+							}).finally(function() {
+								deferred.resolve();
+							});
 
-    $scope.toggleNavMenu = function() {
-        $scope.showNavMenu = !$scope.showNavMenu;
-    }
+		} else {
+			deferred.reject("No data to be saved.");
+		}
+		
+		return deferred.promise;
+	};
+
+	$scope.toggleNavMenu = function() {
+		$scope.showNavMenu = !$scope.showNavMenu;
+	}
 }
