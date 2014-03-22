15d5bbb6968bb148dc644b525aa7c0e5db78b872
Florian Heinemann <florian.heinemann@gmail.com>
2014-03-22 17:53:29 +0100

    Favicon

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ var User = db.model('users', userModel.UserSchema);
 app.set('port', config.http.port);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-app.use(express.favicon());
+app.use(express.favicon('public/images/favicon.ico'));
 app.use(express.logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded());
